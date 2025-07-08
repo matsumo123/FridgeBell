bb6d54fff9bf2b0aae1ecb96e4459a6084f3aeac
matumoto <aiiiii-123@outlook.com>
2025-07-08 20:40:35 +0900

    rubocop修正:Gemfile, config/initializers/content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,19 +7,19 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
-    policy.font_src    :self, :https, 'https://fonts.gstatic.com'
+    policy.font_src    :self, :https, "https://fonts.gstatic.com"
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src  :self, :https
-    policy.style_src   :self, :https, 'https://fonts.googleapis.com'
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
+    policy.style_src   :self, :https, "https://fonts.googleapis.com"
+
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
+  # Generate session nonces for permitted importmap, inline scripts, and inline styles.
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
+
+  # Report violations without enforcing the policy.
+  # config.content_security_policy_report_only = true
 end
