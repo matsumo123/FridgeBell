4d73ba4dba7dbeadc9b55cf7eebeee5c20749a80
matumoto <aiiiii-123@outlook.com>
2025-07-13 19:02:39 +0900

    フラッシュメッセージをi18n対応

--- app/controllers/account/passwords_controller.rb
+++ app/controllers/account/passwords_controller.rb
@@ -6,7 +6,8 @@ class Account::PasswordsController < ApplicationController
   def update
     if current_user.update_with_password(password_params)
       bypass_sign_in current_user
-      redirect_to users_profile_path, notice: "パスワードを変更しました"
+      redirect_to users_profile_path, 
+      notice: t('helpers.flash_messages.password_update')
     else
       render :edit, status: :unprocessable_entity
     end
