fd9d69dda8c548e00144e3d3ee0b719eb5ed1f3f
matumoto <aiiiii-123@outlook.com>
2025-07-14 19:14:12 +0900

    Categoryモデル生成, バリデーションを定義, Categoriesテーブル(name:string)生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_08_115820) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_14_100426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
