0f9b96ddd17196d877477951877b06de5c729863
matumoto <aiiiii-123@outlook.com>
2025-07-29 03:47:30 +0900

    food.default_deadlineがnilの時のdeadline_dateの対応

--- app/controllers/user_foods_controller.rb
+++ app/controllers/user_foods_controller.rb
@@ -1,6 +1,5 @@
 class UserFoodsController < ApplicationController
   before_action :authenticate_user!
-  
   def new
     @categories = Category.order(id: :asc)
     @foods = Food.where(category_id: params[:category_id]).order(name: :asc)
@@ -10,7 +9,7 @@ class UserFoodsController < ApplicationController
   def create
     @user_food = current_user.user_foods.build(user_foods_params)
     if @user_food.save
-      redirect_to  user_foods_path, notice: "冷蔵庫に食材を登録しました"
+      redirect_to user_foods_path, notice: "冷蔵庫に食材を登録しました"
     else
       @categories = Category.order(id: :asc)
       @foods = Food.where(category_id: params[:category_id]).order(name: :asc)
