4a098d5ce12169067eb3e0c8ba2e2683037925c3
matumoto <aiiiii-123@outlook.com>
2025-07-28 17:09:50 +0900

    UserFoodモデル, mini_memoカラムバリデーション修正

--- app/models/user_food.rb
+++ app/models/user_food.rb
@@ -4,5 +4,5 @@ class UserFood < ApplicationRecord
 
   validates :user_id, :food_id, :deadline_date, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :mini_memo, length: { maximum: 50 }
+  validates :mini_memo, length: { maximum: 50 }, allow_nil: true
 end
