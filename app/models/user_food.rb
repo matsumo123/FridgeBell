eb43da7ccce6eaecf9f95a444c2a8bf0b97d8e6f
matumoto <aiiiii-123@outlook.com>
2025-07-28 16:43:26 +0900

    UserFoodモデル生成, バリデーション定義

--- app/models/user_food.rb
+++ app/models/user_food.rb
@@ -1,4 +1,8 @@
 class UserFood < ApplicationRecord
   belongs_to :user
   belongs_to :food
+
+  validates :user_id, :food_id, :deadline_date, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :mini_memo, length: { maximum: 50 }
 end
