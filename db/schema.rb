7f7c942147b3c96035353543cda105f772f6c866
matumoto <aiiiii-123@outlook.com>
2025-07-31 15:08:37 +0900

    food_actionsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_26_134907) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_31_055930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_26_134907) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "food_actions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "user_food_id", null: false
+    t.integer "action_type", null: false
+    t.datetime "action_date", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_food_id"], name: "index_food_actions_on_user_food_id"
+    t.index ["user_id"], name: "index_food_actions_on_user_id"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -91,6 +103,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_26_134907) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "food_actions", "user_foods"
+  add_foreign_key "food_actions", "users"
   add_foreign_key "foods", "categories"
   add_foreign_key "foods", "users"
   add_foreign_key "user_foods", "foods"
