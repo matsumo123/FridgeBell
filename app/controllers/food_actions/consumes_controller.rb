393cbabd2ddea6a2c679c605c3986f70641b39c6
matumoto <aiiiii-123@outlook.com>
2025-08-04 18:24:15 +0900

    food_actionsコントローラー->food_actions/consumesコントローラーに修正, consumeアクション->newアクションに修正

---
+++ app/controllers/food_actions/consumes_controller.rb
@@ -0,0 +1,26 @@
+module FoodActions
+  class ConsumesController < ApplicationController
+    before_action :authenticate_user!
+    def new
+      @user_foods = current_user.user_foods.includes(:food).order(deadline_date: :asc)
+      @form = Form::FoodActionCollection.new(current_user)
+    end
+
+    def create
+      @form = Form::FoodActionCollection.new(current_user, collection_params)
+      if @form.save
+        redirect_to home_path, notice: "食材を消費しました"
+      else
+        flash.now[:alert] = "食材を消費できませんでした"
+        @user_foods = current_user.user_foods.includes(:food).order(deadline_date: :asc)  
+        render :consume, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def collection_params
+      params.require(:form_food_action_collection).permit(food_actions_attributes: [:user_food_id, :action_type, :action_date, :quantity, :availability])
+    end
+  end
+end
