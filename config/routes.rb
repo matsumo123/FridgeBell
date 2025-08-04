cf2ccc7721d18969ad3e6f495e937e5e0857b6ce
matumoto <aiiiii-123@outlook.com>
2025-08-04 18:20:04 +0900

    food_actionsコントローラーをfood_actions/consumes, food_actions/discordsコントローラーに分けてリソースフルなルーティングに修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   end
   resources :foods, only: %i[index new create edit update destroy]
   resources :user_foods, only: %i[index new create edit update destroy]
-  resources :food_actions, only: %i[ create ]
-  get "food_actions/consume" => "food_actions#consume"
-  get "food_actions/discard" => "food_actions#discard"
+  namespace :food_actions do
+    resource :consume, only: %i[new create]
+    resource :discard, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
