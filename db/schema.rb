ae1fc579762012d64392a379932ab5e6df5ba943
matumoto <aiiiii-123@outlook.com>
2025-08-07 01:43:24 +0900

    use_charactersのuser_idをunique: trueに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_06_155119) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_06_163031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,7 +93,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_06_155119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["character_id"], name: "index_user_characters_on_character_id"
-    t.index ["user_id"], name: "index_user_characters_on_user_id"
+    t.index ["user_id"], name: "index_user_characters_on_user_id", unique: true
   end
 
   create_table "user_foods", force: :cascade do |t|
