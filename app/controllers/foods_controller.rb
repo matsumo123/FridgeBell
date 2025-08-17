a786ed465108621d27be1774babd416bf86fadc6
matumoto <aiiiii-123@outlook.com>
2025-08-17 20:15:23 +0900

    foods#index不要のため、ルーティング、コントローラーのindexアクション・関連するファイルを削除

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,6 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
-  before_action :category_id_present?, only: %i[ index ]
   before_action :find_user_food, only: %i[ edit update destroy ]
-
-  def index
-    @categories = Category.order(id: :asc)
-    @foods = Food.where(category_id: params[:category_id]).order(name: :asc)
-  end
 
   def new
     @food = current_user.foods.new(category_id: params[:category_id])
@@ -58,12 +52,6 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :category_id, :quantity, :unit, :default_deadline)
-  end
-
-  def category_id_present?
-    if params[:category_id].blank?
-      redirect_to foods_path(category_id: 1)
-    end
   end
 
   def find_user_food
