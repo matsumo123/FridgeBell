cce8d219d6a3e00d781f8a95672354c7d4170496
matumoto <aiiiii-123@outlook.com>
2025-09-03 21:10:04 +0900

    application_helper.rbに_user_food, _formのbg_color&emojiをまとめた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def bg_color(remaining_days)
+    case remaining_days
+    when ...0
+      "before:bg-gray-400"
+    when 0..2
+      "before:bg-red-400"
+    when 3..5
+      "before:bg-yellow-400"
+    else
+      "before:bg-green-400"
+    end
+  end
+
+  def emoji(remaining_days)
+    case remaining_days
+    when ...0
+      "😵‍💫"
+    when 0
+      "😰"
+    when 1..2
+      "😟"
+    when 3..5
+      "😬"
+    else
+      "☺️"
+    end
+  end 
 end
