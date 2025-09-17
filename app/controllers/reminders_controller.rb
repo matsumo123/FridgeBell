2555e318b5aba38ad42b30559b5d51ab8732cff9
matumoto <aiiiii-123@outlook.com>
2025-09-17 18:29:45 +0900

    feat: reminders_controller update create, show

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,6 +2,7 @@ class RemindersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @reminder = current_user.reminder
   end
 
   def new
@@ -9,5 +10,17 @@ class RemindersController < ApplicationController
   end
 
   def create
+    @reminder = current_user.build_reminder(reminder_params)
+    if @reminder.save
+      redirect_to reminder_path, notice: "リマインド設定が完了しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reminder_params
+    params.require(:reminder).permit(:user_id, :days_before, :remind_time)
   end
 end
