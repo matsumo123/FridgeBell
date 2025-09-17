7ec27c51cedb186cb29e24aeacf32ab8c455950e
matumoto <aiiiii-123@outlook.com>
2025-09-17 14:58:15 +0900

    create: reminders table and Reminder model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_10_093217) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_17_054915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_10_093217) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "reminders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "days_before", null: false
+    t.time "remind_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reminders_on_user_id"
+  end
+
   create_table "user_characters", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "character_id", null: false
@@ -129,6 +138,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_10_093217) do
   add_foreign_key "food_actions", "users"
   add_foreign_key "foods", "categories"
   add_foreign_key "foods", "users"
+  add_foreign_key "reminders", "users"
   add_foreign_key "user_characters", "characters"
   add_foreign_key "user_characters", "users"
   add_foreign_key "user_foods", "foods"
