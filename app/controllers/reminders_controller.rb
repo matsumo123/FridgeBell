c34f2d7a79e63f1678ca8fc0bc935fe43d4454f8
matumoto <aiiiii-123@outlook.com>
2025-09-17 20:40:30 +0900

    feat(reminders): reminders_controllerにedit, updateアクション定義

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,9 +1,8 @@
 class RemindersController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_reminder, only: %i[show edit update destroy]
 
-  def show
-    @reminder = current_user.reminder
-  end
+  def show; end
 
   def new
     @reminder = Reminder.new
@@ -18,8 +17,18 @@ class RemindersController < ApplicationController
     end
   end
 
-  def destroy
+  def edit; end
+
+  def update
     @reminder = current_user.reminder
+    if @reminder.update(reminder_params)
+      redirect_to reminder_path, notice: "リマインド設定を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
     @reminder.destroy!
     redirect_to reminder_path,
                 notice: "リマインド設定を削除しました",
@@ -30,5 +39,9 @@ class RemindersController < ApplicationController
 
   def reminder_params
     params.require(:reminder).permit(:user_id, :days_before, :remind_time)
+  end
+
+  def user_reminder
+    @reminder = current_user.reminder
   end
 end
