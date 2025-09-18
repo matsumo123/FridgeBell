e9a4242a63f328f6cd2bac63eb9a9d6c04c5af07
matumoto <aiiiii-123@outlook.com>
2025-09-18 14:20:10 +0900

    feat(reminders): reminder_modal編集

--- app/javascript/controllers/reminder_modal_controller.js
+++ app/javascript/controllers/reminder_modal_controller.js
@@ -2,6 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reminder-modal"
 export default class extends Controller {
+  static targets = ["reminderModal"]
+
   connect() {
+  }
+
+  close(event){
+    if(event.detail.success){
+      this.reminderModalTarget.classList.add("hidden");
+    }
+  }
+
+  closeModal(){
+    this.reminderModalTarget.classList.add("hidden");
   }
 }
