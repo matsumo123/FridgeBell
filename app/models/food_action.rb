6fe260de80f068dbc4d17b2b3372c4dc55c1388c
matumoto <aiiiii-123@outlook.com>
2025-09-27 19:02:25 +0900

    chore: FoodActionモデルにfood_name, unitカラムのバリデーションを追加

--- app/models/food_action.rb
+++ app/models/food_action.rb
@@ -3,7 +3,9 @@ class FoodAction < ApplicationRecord
   belongs_to :user_food
   enum :action_type, { consumed: 0, discarded: 1 }, validate: true
 
-  validates :user_id, :user_food_id, :action_type, :action_date, :quantity, presence: true
+  validates :user_id, :user_food_id, :action_type, :action_date, :quantity, :food_name, :unit, presence: true
   validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   validates :availability, inclusion: { in: [ true, false ] }
+  validates :food_name, length: { maximum: 15 }
+  validates :unit, length: { maximum: 5 }
 end
