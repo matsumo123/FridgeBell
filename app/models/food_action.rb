e55fac48831eb8bb03a1ccc4d2fc15406ddcb794
matumoto <aiiiii-123@outlook.com>
2025-10-08 15:56:36 +0900

    chore(food_actions): FoodActionのバリデーションにfood_name, unitのpresence: trueを復活

--- app/models/food_action.rb
+++ app/models/food_action.rb
@@ -3,7 +3,7 @@ class FoodAction < ApplicationRecord
   belongs_to :user_food, optional: true
   enum :action_type, { consumed: 0, discarded: 1 }, validate: true
 
-  validates :user_id, :user_food_id, :action_type, :action_date, :quantity, presence: true
+  validates :user_id, :user_food_id, :action_type, :action_date, :quantity, :food_name, :unit, presence: true
   validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   validates :availability, inclusion: { in: [ true, false ] }
   validates :food_name, length: { maximum: 15 }
