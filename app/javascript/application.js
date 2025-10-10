78694f4b5a61be3addb6e9bc0b9a19520952ae5c
matumoto <aiiiii-123@outlook.com>
2025-10-10 16:00:31 +0900

    fix(js): chartjs-plugin-datalabelsのプラグイン登録

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import { Chart } from "chart.js"
+import Chart from "chart.js/auto"
 import ChartDataLabels from "chartjs-plugin-datalabels"
+Chart.register(ChartDataLabels)
 import "chartkick/chart.js"
