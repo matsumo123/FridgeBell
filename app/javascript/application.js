36dbd8b2becc440088b372795b8bbd6092cef1da
matumoto <aiiiii-123@outlook.com>
2025-10-10 17:33:54 +0900

    feat(charts): 円グラフに％ラベル表示／空状態表示／100%時の0%ラベル非表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,4 +4,17 @@ import "./controllers"
 import Chart from "chart.js/auto"
 import ChartDataLabels from "chartjs-plugin-datalabels"
 Chart.register(ChartDataLabels)
+
+Chart.defaults.set("plugins.datalabels", {
+  display: true,
+  color: "#fff",
+  font: { weight: "bold", size: 32 },
+  anchor: "center",
+  align: "center",
+  formatter: (v) => {
+    if(!v || v === 0) return '';
+    return `${v}%`
+  }
+})
+
 import "chartkick/chart.js"
