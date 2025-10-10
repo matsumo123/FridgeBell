abc412872414726542e81bb779f38d4764184377
matumoto <aiiiii-123@outlook.com>
2025-10-10 15:32:31 +0900

    deps: chartjs-plugin-datalabels を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import { Chart } from "chart.js"
+import ChartDataLabels from "chartjs-plugin-datalabels"
 import "chartkick/chart.js"
