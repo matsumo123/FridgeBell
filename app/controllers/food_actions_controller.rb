3b4ec782b4bc72fc439d06c84acd35a71184680a
matumoto <aiiiii-123@outlook.com>
2025-10-11 19:22:04 +0900

    refactor(food_actions): month パラメータ処理を before_action(set_month) に集約

--- app/controllers/food_actions_controller.rb
+++ app/controllers/food_actions_controller.rb
@@ -1,5 +1,6 @@
 class FoodActionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_month, only: %i[history rankings]
   def index
     month_data = FoodAction.this_month(current_user)
     counts = month_data.group(:action_type).count
@@ -12,40 +13,36 @@ class FoodActionsController < ApplicationController
     }
 
     @top_consumed = FoodAction.this_month(current_user).consumed.top3_by_total_quantity
-    
+
     @top_discarded = FoodAction.this_month(current_user).discarded.top3_by_total_quantity
   end
 
   def history
-    if params[:month].present?
-      begin
-        @month = Time.strptime(params[:month], "%Y-%m")
-      rescue ArgumentError
-        @month = Time.current
-      end
-    else
-      @month = Time.current
-    end
     @action_foods = current_user.food_actions.where(action_date: @month.all_month).order(:action_date).page(params[:page]).per(15)
   end
 
   def rankings
-    if params[:month].present?
-      begin
-        @month = Time.strptime(params[:month], "%Y-%m")
-      rescue ArgumentError
-        @month = Time.current
-      end
-    else
-      @month = Time.current
-    end
     @current_tab = %w[consumed discarded].include?(params[:tab]) ? params[:tab] : "consumed"
     base = current_user.food_actions.where(action_date: @month.all_month)
-    scope = 
+    scope =
       case @current_tab
       when "consumed" then base.consumed.ranking
       when "discarded" then base.discarded.ranking
       end
     @action = scope
+  end
+
+  private
+
+  def set_month
+    if params[:month].present?
+      begin
+        @month = Time.strptime(params[:month], "%Y-%m")
+      rescue ArgumentError
+        @month = Time.current
+      end
+    else
+      @month = Time.current
+    end
   end
 end
