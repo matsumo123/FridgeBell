7188e0eb26139401bf174d4aec515f90d8f94d54
matumoto <aiiiii-123@outlook.com>
2025-10-14 16:48:30 +0900

    feat(favorites): お気に入りボタンのajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,12 @@
 class FavoritesController < ApplicationController
  before_action :authenticate_user!
   def create
-    food = Food.find(params[:food_id])
-    current_user.favorite(food)
-    redirect_to new_user_food_path(category_id: params[:category_id], page: params[:page]),
-      notice: "お気に入りに登録しました"
+    @food = Food.find(params[:food_id])
+    current_user.favorite(@food)
   end
 
   def destroy
-    food = current_user.favorites.find(params[:id]).food
-    current_user.unfavorite(food)
-    redirect_to new_user_food_path(category_id: params[:category_id], page: params[:page]),
-      notice: "お気に入りから削除しました"
+    @food = current_user.favorites.find(params[:id]).food
+    current_user.unfavorite(@food)
   end
 end
