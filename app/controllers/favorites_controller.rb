aae6e9d3d005cbb586cbee9c1630a175d324eb67
matumoto <aiiiii-123@outlook.com>
2025-10-14 16:11:16 +0900

    feat(favorites): お気に入り登録・削除のリダイレクト先を変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
   def create
     food = Food.find(params[:food_id])
     current_user.favorite(food)
-    redirect_back fallback_location: new_user_food_path(category_id: 1),
+    redirect_to new_user_food_path(category_id: params[:category_id], page: params[:page]),
       notice: "お気に入りに登録しました"
   end
 
   def destroy
     food = current_user.favorites.find(params[:id]).food
     current_user.unfavorite(food)
-    redirect_back fallback_location: new_user_food_path(category_id: 1),
+    redirect_to new_user_food_path(category_id: params[:category_id], page: params[:page]),
       notice: "お気に入りから削除しました"
   end
 end
