9393b0fc43dac534dfcf7c92a3ae937aa7ea27eb
matumoto <aiiiii-123@outlook.com>
2025-10-18 13:23:10 +0900

    feat(ogp): 静的OGPの表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,34 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'FridgeBell',
+      title: 'FridgeBell | 冷蔵庫の食材を管理するWebアプリ',
+      reverse: true,
+      charset: 'utf-8',
+      description: '期限が切れる前に、あなたにお知らせ',
+      keywords: '冷蔵庫, 食材, 管理, 通知, リマインド',
+      canonical: 'https://fridgebell.fly.dev/',
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('FridgeBell_logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: 'https://fridgebell.fly.dev/',
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('ogp.png')
+      }
+    }
+  end
+
   def bg_color(remaining_days)
     case remaining_days
     when ...0
