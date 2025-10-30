ca9aecfb8440637bedaaba506a2c38b4eec8294b
matumoto <aiiiii-123@outlook.com>
2025-10-30 14:44:07 +0900

    chore(user_food): user_id, food_idのpresence: true削除(belongs_toと重複 しているため)

--- app/models/user_food.rb
+++ app/models/user_food.rb
@@ -3,7 +3,7 @@ class UserFood < ApplicationRecord
   belongs_to :food
   has_many :food_actions, dependent: :nullify
 
-  validates :user_id, :food_id, :deadline_date, presence: true
+  validates :deadline_date, presence: true
   validates :quantity, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   validates :mini_memo, length: { maximum: 50 }, allow_nil: true
 
