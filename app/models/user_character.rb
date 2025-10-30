52a0c37c690878a2a95d8aad3c0b69d971ec8660
matumoto <aiiiii-123@outlook.com>
2025-10-30 14:38:30 +0900

    chore(user_character): user_id, character_idのpresence: true削除(belongs_toと重複しているため)

--- app/models/user_character.rb
+++ app/models/user_character.rb
@@ -1,7 +1,6 @@
 class UserCharacter < ApplicationRecord
   belongs_to :user
   belongs_to :character
-  validates :user_id, :character_id, presence: true
   validates :consecutive_days, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   # last_action_atは今日の日付か
