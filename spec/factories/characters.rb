2a3e4fb0580f2d56af5bb317a8bd49d0156e0fbd
matumoto <aiiiii-123@outlook.com>
2025-11-01 13:34:39 +0900

    chore(factory_bot): CharacterのFactoryに画像添付traitを追加

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory :character do
-    sequence(:stage_number) { |n| n }
+    stage_number { 0 }
     description { "stage description" }
+
+    trait :with_image do
+      after(:build) do |character|
+        file_path = Rails.root.join("db/seeds/images/character/ステージ0.png")
+        character.image.attach(
+          io: File.new(file_path),
+          filename: "ステージ0.png",
+          content_type: "image/png"
+        )
+      end
+    end
   end
 end
